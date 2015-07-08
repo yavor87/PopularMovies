89829de60eb31157b147fc6ce5c71588d0e07ee9
Yavor Ivanov <yavor87@gmail.com>
2015-07-09 00:27:17 +0300

    Initial commit

---
+++ app/src/main/java/com/yavor/popularmovies/MoviesFragment.java
@@ -0,0 +1,22 @@
+package com.yavor.popularmovies;
+
+import android.support.v4.app.ListFragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class MoviesFragment extends ListFragment {
+
+    public MoviesFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_movies, container, false);
+    }
+}
