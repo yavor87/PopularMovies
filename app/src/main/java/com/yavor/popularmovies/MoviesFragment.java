881d9717e5a3cdfe1f2c0d60a8eb5d78ef4f81aa
Yavor Ivanov <yavor87@gmail.com>
2015-07-10 12:42:46 +0300

    Switched to custom adapter

--- app/src/main/java/com/yavor/popularmovies/MoviesFragment.java
+++ app/src/main/java/com/yavor/popularmovies/MoviesFragment.java
@@ -1,15 +1,18 @@
 package com.yavor.popularmovies;
 
+import android.content.Context;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
 import android.widget.GridView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -17,18 +20,17 @@ public class MoviesFragment extends Fragment {
     public MoviesFragment() {
     }
 
-    private ArrayAdapter<String> moviesAdapter;
+    private MovieArrayAdapter mAdapter;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_movies, container, false);
 
-        this.moviesAdapter = new ArrayAdapter<>(this.getActivity(),
-                android.R.layout.simple_list_item_1, new ArrayList<String>());
+        mAdapter = new MovieArrayAdapter(this.getActivity());
 
         GridView gridView = (GridView)rootView.findViewById(R.id.grid_view);
-        gridView.setAdapter(moviesAdapter);
+        gridView.setAdapter(mAdapter);
         gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
@@ -51,13 +53,62 @@ public class MoviesFragment extends Fragment {
         new FetchMoviesTask(new FetchMoviesTask.FetchMoviesCompletionListener() {
             @Override
             public void fetchMoviesCompleted(MovieInfo[] movies) {
-                moviesAdapter.clear();
-                for (MovieInfo m : movies) {
-                    moviesAdapter.add(m.getTitle());
-
-                }
-//            mAdapter.addAdd(movieInfo);
+                mAdapter.reset(movies);
             }
         }).execute(sort_pref);
+    }
+
+    private class MovieArrayAdapter extends BaseAdapter {
+        private final String LOG_TAG = FetchMoviesTask.class.getSimpleName();
+        private final Context mContext;
+        private final ArrayList<MovieInfo> mItemsArrayList;
+
+        public MovieArrayAdapter(Context context) {
+            mContext = context;
+            mItemsArrayList = new ArrayList<>();
+        }
+
+        @Override
+        public int getCount() {
+            return mItemsArrayList.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return mItemsArrayList.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        public void reset(MovieInfo[] movies) {
+            mItemsArrayList.clear();
+            for (MovieInfo m : movies) {
+                mItemsArrayList.add(m);
+            }
+            notifyDataSetChanged();
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            Log.v(LOG_TAG, "getView #" + position);
+            TextView rowView;
+            if (convertView != null)
+            {
+                rowView = (TextView) convertView;
+            }
+            else {
+                LayoutInflater inflater = (LayoutInflater) mContext
+                        .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+                rowView = (TextView) inflater.inflate(android.R.layout.simple_list_item_1, parent, false);
+            }
+
+            rowView.setText(mItemsArrayList.get(position).getPopularity() + "");
+
+            return rowView;
+        }
     }
 }
