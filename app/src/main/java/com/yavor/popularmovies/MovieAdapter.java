ccb873dc4a88610c5d54868483d9cd9363dd7ab8
Yavor Ivanov <yavor87@gmail.com>
2015-07-10 12:52:44 +0300

    Moved MoviesAdapter to upper level. Adding poster retrieval.

---
+++ app/src/main/java/com/yavor/popularmovies/MovieAdapter.java
@@ -0,0 +1,75 @@
+package com.yavor.popularmovies;
+
+import android.content.Context;
+import android.net.Uri;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+
+class MoviesAdapter extends BaseAdapter {
+    public static final String POSTER_GET_BASAE_URL = "http://image.tmdb.org/t/p/";
+    public static final String REQUEST_SIZE = "w185";
+    private static final String LOG_TAG = MoviesAdapter.class.getSimpleName();
+    private final Context mContext;
+    private final ArrayList<MovieInfo> mItemsArrayList;
+
+    public MoviesAdapter(Context context) {
+        mContext = context;
+        mItemsArrayList = new ArrayList<>();
+    }
+
+    @Override
+    public int getCount() {
+        return mItemsArrayList.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return mItemsArrayList.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    public void reset(MovieInfo[] movies) {
+        mItemsArrayList.clear();
+        for (MovieInfo m : movies) {
+            mItemsArrayList.add(m);
+        }
+        notifyDataSetChanged();
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        Log.v(LOG_TAG, "getView #" + position);
+        ImageView rowView;
+        if (convertView != null) {
+            rowView = (ImageView) convertView;
+        } else {
+            rowView = new ImageView(mContext);
+        }
+
+        MovieInfo currentItem = mItemsArrayList.get(position);
+        String posterPath = buildPosterUrl(currentItem);
+        Picasso.with(mContext)
+                .load(posterPath)
+                .into(rowView);
+
+        return rowView;
+    }
+
+    private String buildPosterUrl(MovieInfo info) {
+        return Uri.parse(POSTER_GET_BASAE_URL).buildUpon()
+                .appendPath(REQUEST_SIZE)
+                .appendEncodedPath(info.getPosterPath())
+                .toString();
+    }
+}
--- app/src/main/java/com/yavor/popularmovies/MoviesFragment.java
+++ app/src/main/java/com/yavor/popularmovies/MoviesFragment.java
@@ -1,33 +1,27 @@
 package com.yavor.popularmovies;
 
-import android.content.Context;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
-import android.widget.BaseAdapter;
 import android.widget.GridView;
-import android.widget.TextView;
-
-import java.util.ArrayList;
 
 public class MoviesFragment extends Fragment {
     public MoviesFragment() {
     }
 
-    private MovieArrayAdapter mAdapter;
+    private MoviesAdapter mAdapter;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_movies, container, false);
 
-        mAdapter = new MovieArrayAdapter(this.getActivity());
+        mAdapter = new MoviesAdapter(this.getActivity());
 
         GridView gridView = (GridView)rootView.findViewById(R.id.grid_view);
         gridView.setAdapter(mAdapter);
@@ -56,59 +50,5 @@ public class MoviesFragment extends Fragment {
                 mAdapter.reset(movies);
             }
         }).execute(sort_pref);
-    }
-
-    private class MovieArrayAdapter extends BaseAdapter {
-        private final String LOG_TAG = FetchMoviesTask.class.getSimpleName();
-        private final Context mContext;
-        private final ArrayList<MovieInfo> mItemsArrayList;
-
-        public MovieArrayAdapter(Context context) {
-            mContext = context;
-            mItemsArrayList = new ArrayList<>();
-        }
-
-        @Override
-        public int getCount() {
-            return mItemsArrayList.size();
-        }
-
-        @Override
-        public Object getItem(int position) {
-            return mItemsArrayList.get(position);
-        }
-
-        @Override
-        public long getItemId(int position) {
-            return 0;
-        }
-
-        public void reset(MovieInfo[] movies) {
-            mItemsArrayList.clear();
-            for (MovieInfo m : movies) {
-                mItemsArrayList.add(m);
-            }
-            notifyDataSetChanged();
-        }
-
-        @Override
-        public View getView(int position, View convertView, ViewGroup parent) {
-            Log.v(LOG_TAG, "getView #" + position);
-            TextView rowView;
-            if (convertView != null)
-            {
-                rowView = (TextView) convertView;
-            }
-            else {
-                LayoutInflater inflater = (LayoutInflater) mContext
-                        .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-
-                rowView = (TextView) inflater.inflate(android.R.layout.simple_list_item_1, parent, false);
-            }
-
-            rowView.setText(mItemsArrayList.get(position).getPopularity() + "");
-
-            return rowView;
-        }
     }
 }
