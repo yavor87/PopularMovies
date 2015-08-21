65d0fb332774ed0c2977e09f08580382c51fe0ce
Yavor <yavor87@gmail.com>
2015-08-21 19:26:36 +0300

    Adding some provider tests

--- app/src/androidTest/java/com/yavor/popularmovies/data/MoviesProviderTests.java
+++ app/src/androidTest/java/com/yavor/popularmovies/data/MoviesProviderTests.java
@@ -2,6 +2,7 @@ package com.yavor.popularmovies.data;
 
 import android.content.ContentUris;
 import android.content.ContentValues;
+import android.database.Cursor;
 import android.net.Uri;
 
 import com.yavor.popularmovies.ApplicationTest;
@@ -63,6 +64,49 @@ public class MoviesProviderTests extends ApplicationTest {
         // Assert
         assertNotNull(uri);
         assertEquals(1, ContentUris.parseId(uri));
+    }
+
+    public void testQuery_Movies() {
+        // Arrange
+        ContentValues values = getTestMovie();
+        getContext().getContentResolver().insert(MoviesContract.Movie.CONTENT_URI, values);
+
+        // Act
+        Cursor c = getContext().getContentResolver().query(MoviesContract.Movie.CONTENT_URI, null, null, null, null);
+
+        // Assert
+        assertTrue(c.moveToFirst());
+        assertEquals(1, c.getInt(c.getColumnIndex(MoviesContract.Movie._ID)));
+        assertEquals(values.get(MoviesContract.Movie.TITLE), c.getString(c.getColumnIndex(MoviesContract.Movie.TITLE)));
+        assertEquals(values.get(MoviesContract.Movie.OVERVIEW), c.getString(c.getColumnIndex(MoviesContract.Movie.OVERVIEW)));
+        assertEquals(values.get(MoviesContract.Movie.POSTERPATH), c.getString(c.getColumnIndex(MoviesContract.Movie.POSTERPATH)));
+        assertEquals(values.get(MoviesContract.Movie.RELEASEDATE), c.getLong(c.getColumnIndex(MoviesContract.Movie.RELEASEDATE)));
+        assertEquals(values.get(MoviesContract.Movie.RUNTIME), c.getInt(c.getColumnIndex(MoviesContract.Movie.RUNTIME)));
+        assertEquals(values.getAsFloat(MoviesContract.Movie.VOTEAVERAGE), c.getFloat(c.getColumnIndex(MoviesContract.Movie.VOTEAVERAGE)));
+
+        c.close();
+    }
+
+    public void testQuery_SingleMovie() {
+        // Arrange
+        ContentValues values = getTestMovie();
+        getContext().getContentResolver().insert(MoviesContract.Movie.CONTENT_URI, values);
+
+        // Act
+        Cursor c = getContext().getContentResolver()
+                .query(ContentUris.withAppendedId(MoviesContract.Movie.CONTENT_URI, 1), null, null, null, null);
+
+        // Assert
+        assertTrue(c.moveToFirst());
+        assertEquals(1, c.getInt(c.getColumnIndex(MoviesContract.Movie._ID)));
+        assertEquals(values.get(MoviesContract.Movie.TITLE), c.getString(c.getColumnIndex(MoviesContract.Movie.TITLE)));
+        assertEquals(values.get(MoviesContract.Movie.OVERVIEW), c.getString(c.getColumnIndex(MoviesContract.Movie.OVERVIEW)));
+        assertEquals(values.get(MoviesContract.Movie.POSTERPATH), c.getString(c.getColumnIndex(MoviesContract.Movie.POSTERPATH)));
+        assertEquals(values.get(MoviesContract.Movie.RELEASEDATE), c.getLong(c.getColumnIndex(MoviesContract.Movie.RELEASEDATE)));
+        assertEquals(values.get(MoviesContract.Movie.RUNTIME), c.getInt(c.getColumnIndex(MoviesContract.Movie.RUNTIME)));
+        assertEquals(values.getAsFloat(MoviesContract.Movie.VOTEAVERAGE), c.getFloat(c.getColumnIndex(MoviesContract.Movie.VOTEAVERAGE)));
+
+        c.close();
     }
 
     private ContentValues getTestMovie() {
